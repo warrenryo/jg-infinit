59f5b538a6d9ce7c136d976e597545877d9448d3
unknown <ralphmatthewmanabat@gmail.com>
2024-04-24 14:48:31 +0700

    change nav

--- frontend/src/components/landingpage/Navbar.jsx
+++ frontend/src/components/landingpage/Navbar.jsx
@@ -183,7 +183,13 @@ const Navbar = () => {
                 </div>
               </div> */}
 
-              <div>
+
+              <a
+                className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500"
+                href="#"
+              >
+
+<div>
                 <a href="#quote"
                   type="button"
                   className="py-2 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary_red text-white hover:bg-red-800 disabled:opacity-50 disabled:pointer-events-none"
@@ -191,22 +197,6 @@ const Navbar = () => {
                   GET A FREE QUOTE
                 </a>
               </div>
-
-              <a
-                className="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500"
-                href="#"
-              >
-                <svg
-                  className="flex-shrink-0 size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                </svg>
-                Log in
               </a>
             </div>
           </div>
